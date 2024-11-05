1809eb221b73e923e343b42126c5fe42ab61dff6
Ron Pressler <ron.pressler@oracle.com>
2024-11-05 17:19:05 +0000

    Fix return type comparison

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
@@ -2110,9 +2110,12 @@ public class Check {
                         (s2.flags() & SYNTHETIC) != 0) continue;
                 if (st1 == null) st1 = types.memberType(t1, s1);
                 Type st2 = types.memberType(t2, s2);
+                Type _st1 = st1, _st2 = st2;
                 if (types.overrideEquivalent(st1, st2)
-                        || types.overrideEquivalent(types.eraseThrowsParam(st1), st2)
-                        || types.overrideEquivalent(st1, types.eraseThrowsParam(st2))) {
+                        || types.overrideEquivalent((_st1 = types.eraseThrowsParam(st1)), st2)
+                        || types.overrideEquivalent(st1, (_st2 = types.eraseThrowsParam(st2)))) {
+                    st1 = _st1;
+                    st2 = _st2;
                     List<Type> tvars1 = st1.getTypeArguments();
                     List<Type> tvars2 = st2.getTypeArguments();
                     Type rt1 = st1.getReturnType();
